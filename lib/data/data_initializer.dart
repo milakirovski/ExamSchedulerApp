e39477bafe09175a0cf14a124bc61b2f2b2104f7
Mila Kirovski <kirovski.mila@gmail.com>
2025-11-11 00:09:50 +0100

    refactor(exam id): changed the id of the exam
    - changed the id of the exam to be the code of the course.

--- lib/data/data_initializer.dart
+++ lib/data/data_initializer.dart
@@ -2,85 +2,85 @@ import '../models/exam_model.dart';
 
 List<Exam> sampleExams = [
   Exam(
-    id: 1,
+    id: "F18L3W128",
     course: "Мобилни и Информациски системи",
     date: DateTime(2025, 12, 15, 10, 0), // Future exam
     rooms: ["Лаб 1", "Лаб 2"],
   ),
   Exam(
-    id: 2,
+    id: "F18L3W024",
     course: "Веб Програмирање",
     date: DateTime(2025, 11, 5, 13, 30), // Past exam (Current date is Nov 10)
     rooms: ["Амфитеатар 1"],
   ),
   Exam(
-    id: 3,
+    id: "F18L3W004",
     course: "Бази на Податоци",
     date: DateTime(2026, 1, 20, 9, 0), // Future exam
     rooms: ["Лаб 3", "218"],
   ),
   Exam(
-    id: 4,
+    id: "F18L2W001",
     course: "Алгоритми и Податочни струткури",
     date: DateTime(2025, 10, 28, 16, 0), // Past exam
     rooms: ["111", "112", "113"],
   ),
   Exam(
-    id: 5,
+    id: "F18L2S029",
     course: "Софтверско Инженерство",
     date: DateTime(2025, 12, 17, 12, 0), // Future exam
     rooms: ["Онлајн"],
   ),
   Exam(
-    id: 6,
+    id: "F18L2W014",
     course: "Компјутерски Мрежи и Безбедност",
     date: DateTime(2026, 1, 22, 15, 0), // Future exam
     rooms: ["220"],
   ),
   Exam(
-    id: 7,
+    id: "F18L2S017",
     course: "Оперативни Системи",
     date: DateTime(2025, 11, 8, 11, 0), // Past exam
     rooms: ["Амфитеатар 2"],
   ),
   Exam(
-    id: 8,
+    id: "F18L3W008",
     course: "Вовед во Наука за Податоци",
     date: DateTime(2026, 2, 2, 14, 0), // Future exam
     rooms: ["Лаб 5", "Лаб 6"],
   ),
   Exam(
-    id: 9,
+    id: "F18L3S155",
     course: "Сервисно Ориентирани Архитектури",
     date: DateTime(2025, 12, 1, 10, 0), // Future exam
     rooms: ["Голем Амфитеатар"],
   ),
   Exam(
-    id: 10,
+    id: "F18L3S025",
     course: "Електронска и Мобилна Трговија",
     date: DateTime(2025, 9, 15, 9, 0), // Past exam
     rooms: ["Библиотека"],
   ),
   Exam(
-    id: 11,
+    id: "F18L3S010",
     course: "Дизајн на Интеракција Човек-Компјутер",
     date: DateTime(2026, 1, 29, 17, 0), // Future exam
     rooms: ["215", "216"],
   ),
   Exam(
-    id: 12,
-    course: "Математика 1",
+    id: "F18L1W033",
+    course: "Калкулус 1",
     date: DateTime(2025, 12, 22, 12, 0), // Future exam
     rooms: ["117", "119"],
   ),
   Exam(
-    id: 12,
-    course: "Калкулус",
+    id: "F18L1S034",
+    course: "Калкулус 2",
     date: DateTime(2025, 12, 22, 12, 0), // Future exam
     rooms: ["117", "119"],
   ),
   Exam(
-    id: 12,
+    id: "F18L1W011",
     course: "Дискретна Математика",
     date: DateTime(2025, 12, 22, 12, 0), // Future exam
     rooms: ["117", "119"],
